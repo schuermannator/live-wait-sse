5881dc494e02b471480395d69f9287867dd58eb0
Zach Schuermann <zachary.zvs@gmail.com>
2020-03-15 15:26:08 -0500

    updates + cleanup

--- client/src/index.js
+++ client/src/index.js
@@ -3,26 +3,15 @@ import "./styles.css"
 var httpURL = "http://localhost:8080/";
 
 window.addEventListener("load", function(evt) {
-    //var output = document.getElementById("output");
     var input = document.getElementById("input");
     var status = document.getElementById("status");
     var line = document.getElementById("line");
     var joiner = document.getElementById("joiner");
     var leaver = document.getElementById("leaver");
     var greet = document.getElementById("greet");
-    //var ws;
 
-    //var print = function(message) {
-    //    var d = document.createElement("div");
-    //    d.innerHTML = message;
-    //    output.appendChild(d);
-    //}
-
-    //ws = new WebSocket(wsURL);
     const evtSource = new EventSource("//localhost:8080/sse");
-
-    //console.log(evtSource.withCredentials); (false rn)
-    //console.log(evtSource.url);
+    var last = evtSource.readyState;
 
     if (localStorage.getItem('name') != null) {
         joiner.classList.add("hidden");
@@ -34,9 +23,8 @@ window.addEventListener("load", function(evt) {
     }
 
     var getStatus = function() {
-        console.log(evtSource.readyState);
         var stat = ""
-        switch(evtSource.readyState) {
+        switch(evtSource.readyState + last) {
             case 1:
                 stat += "OPEN"
                 break;
@@ -48,13 +36,13 @@ window.addEventListener("load", function(evt) {
             joiner.classList.remove("hidden");
             leaver.classList.add("hidden");
         }
+        last = evtSource.readyState;
     }
 
     var updateLine = function(linedata) {
         var data = linedata;
         if (!data)
             return;
-        console.log(data)
         var entries = "";
         var myname = localStorage.getItem('name');
         for (name in data) {
@@ -76,62 +64,43 @@ window.addEventListener("load", function(evt) {
     }
 
     evtSource.onmessage = function(evt) {
-        //print("OPEN");
-        console.log("GOT EVT");
-        console.log(evt);
-        console.log(JSON.parse(evt.data));
         getStatus();
         updateLine(JSON.parse(evt.data));
     }
-    //ws.onclose = function(evt) {
-    //    //print("CLOSE");
-    //    getStatus();
-    //    ws = null;
-    //}
-    //ws.onmessage = function(evt) {
-    //    //print("RESPONSE: " + evt.data);
-    //    getStatus();
-    //    updateLine(JSON.parse(evt.data));
-    //}
-    //ws.onerror = function(evt) {
-    //    //print("ERROR: " + evt.data);
-    //    getStatus();
-    //}
+
+    evtSource.onopen = function(evt) {
+        getStatus();
+    }
+
+    evtSource.onerror = function(evt) {
+        getStatus();
+    }
+
     document.getElementById("join").onclick = function(evt) {
         if (localStorage.getItem('name') == null) {
-            //print("SEND: " + input.value);
-            //ws.send(input.value);
             fetch(httpURL+'push?event='+input.value, {
                 method: 'PUT',
+            }).then(function(response) {
+                localStorage.setItem('name', input.value);
+                joiner.classList.add("hidden");
+                leaver.classList.remove("hidden");
+                greet.innerHTML = "Hi, " + input.value + " you are in the queue."
+                return false;
             });
-            // move this
-            localStorage.setItem('name', input.value);
-            joiner.classList.add("hidden");
-            leaver.classList.remove("hidden");
-            greet.innerHTML = "Hi, " + input.value + " you are in the queue."
-            return false;
         }
     };
 
     document.getElementById("leave").onclick = function(evt) {
-        //if (!ws) {
-            //return false;
-        //}
         var name = localStorage.getItem('name');
         if (name != null) {
-            //print("Leave: " + name);
             fetch(httpURL+'leave?event='+name, {
                 method: 'PUT',
             }).then(function(response) {
-                response.text().then(function(text) {
-                    //console.log(text);
-                });
+                localStorage.removeItem('name');
+                joiner.classList.remove("hidden");
+                leaver.classList.add("hidden");
+                return false;
             });
-            // move this
-            localStorage.removeItem('name');
-            joiner.classList.remove("hidden");
-            leaver.classList.add("hidden");
-            return false;
         }
     };
 });
--- src/main.rs
+++ src/main.rs
@@ -17,13 +17,7 @@ use rocket::response::Response;
 use rocket::State;
 use rocket::http::{ContentType, Status};
 use rocket::response::Responder;
-use crossbeam::queue::SegQueue;
-use serde::{Serialize, Deserialize};
 
-//#[derive(Serialize, Deserialize)]
-//struct WaitQueue(SegQueue<String>);
-//#[derive(Debug)]
-//struct WaitQueue(VecDeque<String>);
 struct WaitQueue(Arc<RwLock<VecDeque<String>>>);
 
 //struct SSEresp(String);
@@ -41,16 +35,13 @@ impl Responder<'static> for SSEresp {
 }
 
 #[put("/push?<event>")]
-//fn push(event: String, queue: VecDeque<String>) {
 fn push(event: String, queue: State<WaitQueue>) {
     queue.0.write().unwrap().push_back(event);
-    //queue.0.push(event);
 }
 
 #[get("/pop")]
 fn pop(queue: State<WaitQueue>) -> Option<String> {
     queue.0.write().unwrap().pop_front()
-    //queue.0.pop().ok()
 }
 
 #[put("/leave?<event>")]
@@ -59,19 +50,11 @@ fn leave(event: String, queue: State<WaitQueue>) {
     q.retain(|x| x != &event);
 }
 
-#[get("/sse")]//, format = "text/event-stream")]
+#[get("/sse")]
 fn sse(queue: State<WaitQueue>) -> SSEresp {
-    //let ser = serde_json::to_string(&queue.0).unwrap();
-    //queue.0.push_back(String::from("HHI"));
-    //queue.0.push_back(String::from("HI"));
     let q = &queue.0;
     let data =  serde_json::to_string(&q.read().unwrap().iter().collect::<Vec<&String>>()).unwrap(); 
-
-    //let q = &queue;
-    println!("data vec: {}\n\n", data);
-    //let data = r#"{ "res": ["test1", "test2"]}"#;
     SSEresp { data: format!("data: {}\n\n", data) }
-    //SSEresp(String::from(format!("data: {}\n\n", 45)))
 }
 
 #[get("/<file..>")]
@@ -84,26 +67,9 @@ fn index() -> Option<NamedFile> {
     NamedFile::open(Path::new("client/dist/index.html")).ok()
 }
 
-
-//#[get("/sse", format = "text/event-stream")]
-//fn sse() {
-//    let mut resp = Response::new();
-//    let sse_content_type = ContentType::new("text", "event-stream");
-//
-//    resp.set_header(sse_content_type);
-//    resp.adjoin_raw_header("Cache-Control", "no-cache");
-//    resp.adjoin_raw_header("Connection", "keep-alive");
-//
-//    resp.set_sized_body(Cursor::new("idk"));
-//    resp.finalize();
-//    resp.ok()
-//}
-
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
         .mount("/", routes![index, leave, files, sse, push, pop])
-        //.manage(WaitQueue(SegQueue::new()))
-        //.manage(WaitQueue(VecDeque::new()))
         .manage(WaitQueue(Arc::new(RwLock::new(VecDeque::new()))))
 }
 
