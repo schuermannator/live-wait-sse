589d11f0fd3fc1796e64770e430c6430515dc8e7
Zach Schuermann <zachary.zvs@gmail.com>
2020-09-30 00:20:09 -0500

    gonna need to clean this one up in the morning

--- live_wait_server/src/main.rs
+++ live_wait_server/src/main.rs
@@ -1,5 +1,3 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use]
 extern crate rocket;
 
@@ -16,15 +14,26 @@ use rocket::response::NamedFile;
 use rocket::State;
 use rocket_contrib::json::Json;
 
-use futures_util::stream::Stream;
+// use futures_util::stream::Stream;
 use live_wait_server::sse;
 use tokio::sync::broadcast;
 
 use live_wait_server::Student;
 
+use crate::rocket::futures::TryStreamExt;
+
+use rocket::{get, launch, routes};
+//  use rocket::request::{Form, State};
+//  use rocket::response::NamedFile;
+//
+use rocket::tokio::stream::{Stream, StreamExt};
+//  use rocket::tokio::sync::broadcast;
+
+use crate::sse::{EventSource, Message as SSEMessage};
+
 struct WaitQueue(RwLock<VecDeque<Student>>);
 
-fn send_broadcast(queue: State<WaitQueue>, subqueue: State<'_, broadcast::Sender<Vec<Student>>>) {
+fn send_broadcast(queue: State<WaitQueue>, subqueue: &State<'_, broadcast::Sender<Vec<Student>>>) {
     let reader = queue.0.read().unwrap();
     let mut to_send = vec![];
     for s in reader.iter() {
@@ -44,7 +53,7 @@ fn add(
         comment: String::from(""),
         join_time: Utc::now(),
     });
-    send_broadcast(queue, subqueue);
+    send_broadcast(queue, &subqueue);
 }
 
 #[put("/push", format = "json", data = "<joinstudent>")]
@@ -56,13 +65,13 @@ fn push(
     let mut student = joinstudent.0;
     student.join_time = Utc::now();
     queue.0.write().unwrap().push_back(student);
-    send_broadcast(queue, subqueue);
+    send_broadcast(queue, &subqueue);
 }
 
 #[get("/pop")]
 fn pop(queue: State<WaitQueue>, subqueue: State<'_, broadcast::Sender<Vec<Student>>>) -> String {
     let name = queue.0.write().unwrap().pop_front().unwrap().name;
-    send_broadcast(queue, subqueue);
+    send_broadcast(queue, &subqueue);
     name
 }
 
@@ -76,56 +85,77 @@ fn leave(
         let mut q = queue.0.write().unwrap();
         q.retain(|x| x.name != event);
     }
-    send_broadcast(queue, subqueue);
+    send_broadcast(queue, &subqueue);
 }
 
 #[get("/sse")]
 async fn sse(
     wq: State<'_, WaitQueue>,
     queue: State<'_, broadcast::Sender<Vec<Student>>>,
-) -> sse::SSE2<impl Stream<Item = sse::Event>> {
+) -> sse::EventSource<impl Stream<Item = sse::Message>> {
     // Subscribe to messages
-    let mut subscription = queue.subscribe();
+    // let mut subscription = queue.subscribe();
 
-    // Create the SSE stream
-    // TODO: Only need async_stream here because subscription does not implement Stream
-    let stream = async_stream::stream! {
-        loop {
-            match subscription.recv().await {
+    // // Create the SSE stream
+    // // TODO: Only need async_stream here because subscription does not implement Stream
+    // let stream = async_stream::stream! {
+    //     loop {
+    //         match subscription.recv().await {
+    //             Ok(student_vec) => {
+    //                 let data = serde_json::to_string(&student_vec).unwrap();
+    //                 yield sse::Message::new(Some("message".into()), Some(data.into()), None);
+    //             }
+    //             Err(broadcast::RecvError::Closed) => break,
+    //             Err(broadcast::RecvError::Lagged(_)) => {
+    //                 yield sse::Message::new(Some("behind".into()), None, None);
+    //             }
+    //         }
+    //     }
+    // };
+
+    send_broadcast(wq, &queue);
+
+    // sse::from_stream(stream)
+
+    // Subscribe to messages and map it to an SSE stream
+    EventSource::from_stream(
+        queue
+            .subscribe()
+            .into_stream()
+            .take_while(|item| !matches!(item, Err(broadcast::RecvError::Closed)))
+            .map(|item| match item {
                 Ok(student_vec) => {
-                    let data = serde_json::to_string(&student_vec).unwrap();
-                    yield sse::Event::new(Some("message".into()), Some(data.into()), None);
+                    SSEMessage::build()
+                        .event("message")
+                        // Format messages as "room\n<user> message".
+                        // TODO: We already checked that 'room' has no newlines or anything else weird
+                        .data(serde_json::to_string(&student_vec).unwrap())
+                        .into()
                 }
-                Err(broadcast::RecvError::Closed) => break,
-                Err(broadcast::RecvError::Lagged(_)) => {
-                    yield sse::Event::new(Some("behind".into()), None, None);
-                }
-            }
-        }
-    };
-
-    send_broadcast(wq, queue);
-
-    sse::from_stream(stream)
+                Err(broadcast::RecvError::Closed) => unreachable!(),
+                Err(broadcast::RecvError::Lagged(_)) => SSEMessage::build().event("behind").into(),
+            }),
+    )
 }
 
 #[get("/<file..>")]
-fn files(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new("client/dist/").join(file)).ok()
+async fn files(file: PathBuf) -> NamedFile {
+    NamedFile::open(Path::new("client/dist/").join(file))
+        .await
+        .expect("to get file")
 }
 
 #[get("/")]
-fn index() -> Option<NamedFile> {
-    NamedFile::open(Path::new("client/dist/index.html")).ok()
+async fn index() -> NamedFile {
+    NamedFile::open(Path::new("client/dist/index.html"))
+        .await
+        .expect("index.html")
 }
 
+#[launch]
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
         .mount("/", routes![index, add, leave, files, sse, push, pop])
         .manage(WaitQueue(RwLock::new(VecDeque::new())))
         .manage(broadcast::channel::<Vec<Student>>(1024).0)
-}
-
-fn main() {
-    rocket().launch().unwrap();
 }
